669a654db0a103b3e350d1f46dc3957789a19f6b
Dan Bennett <bennetdj@rose-hulman.edu>
2011-03-28 09:31:50 -0400

    updated my name

--- helloBeagle.c
+++ helloBeagle.c
@@ -26,6 +26,7 @@ int main(int argc, char **argv)
   printf("Hi this is Brian Hulette\n"); 
   printf("Greetings from Joel Carlson\n");
   printf("Mike Fuson was here\n");
+  printf("-->Dan Bennett<--\n");
 
   return 0;
 }
