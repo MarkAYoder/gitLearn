d74cf89532613e9daeaf7cc9564e1e8e4fca1061
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2012-06-07 04:48:16 +0100

    What does this do?

--- helloWorld.c
+++ helloWorld.c
@@ -16,6 +16,7 @@ int main(int argc, char **argv)
   printf("\n");
   printf("Hi this is Mark A. Yoder\n");
   printf("Yoder was here!");
+  printf("What happens if I ad this?\n");
 
   printf("Put your name here.\n");
 
