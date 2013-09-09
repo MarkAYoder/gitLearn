73aac7a9d60b3f8633e3396bac589b0c6561e4f8
elswicwj <elswicwj@rose-hulman.edu>
2013-09-09 15:13:59 -0400

    Added "Will Elswick" to printout.

--- helloWorld.c
+++ helloWorld.c
@@ -24,6 +24,7 @@ int main(int argc, char **argv)
 	printf("And Eric Ames!\n");
 
   printf("Hi this is James Savage\n");
+  printf("Hi this is Will Elswick\n");
 
   // Add your name here.  Be sure it still compiles.
   printf("Hey, this is Mike McDonald!!!\n");
