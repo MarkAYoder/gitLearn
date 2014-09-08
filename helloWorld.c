07978abf6b029a0cadd3417b05a38a43f865cffc
zhihao xue <xuezhihao@gmail.com>
2014-09-08 14:20:11 -0400

    add my name Zhihao Xue

--- helloWorld.c
+++ helloWorld.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
   printf("\n");
   printf("Hi this is Mark A. Yoder\n");
   // Add your name here.  Be sure it still compiles.
+  printf("My name is Zhihao Xue\n");
  
   return 0;
 }
