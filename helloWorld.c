d4aa987f743446a0475b987191055c2ecf84bf48
unknown <wiizeldaguy@gmail.com>
2014-09-08 14:44:57 -0400

    added my name

--- helloWorld.c
+++ helloWorld.c
@@ -19,6 +19,7 @@ int main(int argc, char **argv)
   printf("hi, i'm weijian \n"); 
   // Add your name here.  Be sure it still compiles.
   printf("Hey from Dmitry Votintsev\n"); 
+  printf("Hey from Matthew Olson\n"); 
   printf("My name is Zhihao Xue\n");
   printf("This is Eric Taylor\n"); 
   printf("Me again\n");
