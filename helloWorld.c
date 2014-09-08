a199401024c4cb2631e34383b91c9773854a20fc
Eric Taylor <taylorem@rose-hulman.edu>
2014-09-08 14:15:39 -0400

    ET change

--- helloWorld.c
+++ helloWorld.c
@@ -17,6 +17,6 @@ int main(int argc, char **argv)
   printf("\n");
   printf("Hi this is Mark A. Yoder\n");
   // Add your name here.  Be sure it still compiles.
- 
+  printf("This is Eric Taylor\n"); 
   return 0;
 }
