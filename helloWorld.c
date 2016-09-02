63bf70843e39cbdeab85e529c9dc342f0217299c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-09-02 15:07:03 -0400

    Added my name

--- helloWorld.c
+++ helloWorld.c
@@ -28,6 +28,7 @@ int main(int argc, char **argv)
   printf("This is david. caggiano. Lord of atlantis\n");
   printf("This is David Mehl\n");
   printf("Hi this is Boyu Zhang\n");
+  printf("Hello this is Tim Smith\n");
 
 
   return 0;
