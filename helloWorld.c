5cada8d164771828d156e217c22b0d1972a1198e
root <root@beaglebone.localdomain>
2016-09-02 18:29:51 +0000

    added my name

--- helloWorld.c
+++ helloWorld.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
   printf("\n");
   printf("Hi this is Mark A. Yoder\n");
   // Add your name below here.  Be sure it still compiles.
+  printf("Hi this is Josh Woods\n"); 
 
   return 0;
 }
