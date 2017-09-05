aec7f53fb14c392c5df38c79f96524f28bd13aca
Adam Bretsch <adambretsch@gmail.com>
2017-09-05 09:34:15 -0400

    Added my own name

--- helloWorld.c
+++ helloWorld.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
   printf("Hi this is Mark A. Yoder\n");
   printf("Hi this is Ryan Evans\n");
   // Add your name below here.  Be sure it still compiles.
-
+  printf("Hi this is Adam Bretsch\n");
   printf("Hello I am Andrew Mueller\n");
   printf("Hello I am Adam Wiesler\n");
   printf("Hi this is Daniel Neelappa\n");
