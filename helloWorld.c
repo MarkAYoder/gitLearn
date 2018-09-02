48811e95065cc4a2b6398d2d17ae0a88812e3fae
Demo User <debian@beaglebone.localdomain>
2018-09-02 17:54:36 +0000

    Added my name

--- helloWorld.c
+++ helloWorld.c
@@ -24,5 +24,6 @@ int main(int argc, char **argv)
   printf("Hi this is Matthew Howlett\n");
   printf("Hi this is Joseph A. Lemmond\n");
   printf("Hi this is Manoj Kurapati\n");
+  printf("Hi this is Brian Jennings\n");
   return 0;
 }
