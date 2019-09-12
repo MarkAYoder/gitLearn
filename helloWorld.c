4f9e91739df548962aac20cf157502b96d25d43e
Mark Procter <mprocter12@gmail.com>
2019-09-12 11:07:23 -0400

    Added name to helloworld.c

--- helloWorld.c
+++ helloWorld.c
@@ -23,6 +23,7 @@ int main(int argc, char **argv)
   printf("Hi this is Brock Grinstead\n");
   printf("Hi this is Eric Fast\n");
   printf("Hi this is Andy Barbour\n")
-  printf("Hi this is Seun Ladipo\n);
+  printf("Hi this is Seun Ladipo\n");
+  printf("Hi this is Mark Procter\n");
   return 0;
 }
