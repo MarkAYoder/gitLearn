c53c8767d13cd6c9b00ca413a5a38dd8bc4fd7ef
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-09-03 13:17:07 -0400

    Added python version

---
+++ helloWorld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+LED="P9_11"
+
+GPIO.setup(LED, GPIO.OUT)
+
+print("Hello, World!")
+
+print("\r")
+
+print("Hi, this is Mark A. Yoder")
+# Add your name below here.  Be sure it still runs.
